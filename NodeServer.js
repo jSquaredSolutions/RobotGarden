ec1e7e18567ead9da48748fbc728c34460aa150c
jsquared <info@jsquaredsolutions.net>
2020-10-23 21:16:01 +0100

    added more files to web server

--- NodeServer.js
+++ NodeServer.js
@@ -43,6 +43,36 @@ var server = http.createServer(function (request, response) {
                 }
             });
             break;
+            case '/Humid.json':
+                fs.readFile(__dirname + path, function (error, data) {
+                    if (error) {
+                        response.writeHead(404);
+                        response.write(error);
+                        response.end();
+                    } else {
+                        response.writeHead(200, {
+                            'Content-Type': 'text/html'
+                        });
+                        response.write(data);
+                        response.end();
+                    }
+                });
+                break;
+            case '/MoistureData.json':
+                fs.readFile(__dirname + path, function (error, data) {
+                    if (error) {
+                        response.writeHead(404);
+                        response.write(error);
+                        response.end();
+                    } else {
+                        response.writeHead(200, {
+                            'Content-Type': 'text/html'
+                        });
+                        response.write(data);
+                        response.end();
+                    }
+                });
+                break;
         case '/images.html':
             fs.readFile(__dirname + path, function (error, data) {
                 if (error) {
